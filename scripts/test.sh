3eda9559cda51f5746d02e317a13781d35f4ad5f
Shaked Shauli <shaked.s@sealsecurity.io>
2024-04-01 17:57:28 +0300

    all

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,11 @@
-#!/bin/bash -ex
+#!/bin/bash
+
+set -ev
 
 sudo cat /etc/apt/sources.list
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo "deb http://deb.debian.org/debian bookworm-backports main contrib non-free" > sudo tee /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian bookworm-backports main contrib non-free" > sources.list
+sudo cp sources.list /etc/apt/sources.list
 sudo cat /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9 6ED0E7B82643E131
 sudo apt-get update
