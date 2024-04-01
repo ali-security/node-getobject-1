ee2f16bd7858744741fcacaad6528888abf0f5de
Shaked Shauli <shaked.s@sealsecurity.io>
2024-04-01 18:16:29 +0300

    all

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,3 +11,5 @@ sudo ls -la /etc/apt/sources.list.d
 sudo rm -rf /etc/apt/sources.list.d/*
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9 6ED0E7B82643E131
 sudo apt-get update
+openssl version
+sudo git config --global --add http.sslVersion tlsv1.2
